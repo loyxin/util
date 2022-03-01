27d9671294dee0d140f603b4bb865c10fbf8f29b
Loyxin <loy.xin@qq.com>
2022-03-01 23:50:37 +0800

    add factory in sington

--- common/design/singleton/Singleton.h
+++ common/design/singleton/Singleton.h
@@ -8,12 +8,30 @@
 
 namespace util::common::design {
 
+namespace detail {
+
+template <class T>
+class Delete {
+public:
+    Delete() = default;
+    ~Delete() = default;
+    void operator()(T* ptr)
+    {
+        if (m_own) {
+            delete ptr;
+        }
+    }
+    bool m_own = true;
+};
+
+}  // namespace detail
+
 template <typename T>
 class Singleton final {
 public:
     Singleton() = delete;
     ~Singleton() = delete;
-	NONCOPYMOVE(Singleton);
+    NONCOPYMOVE(Singleton);
 
     static T* getPtr()
     {
@@ -27,6 +45,13 @@ public:
         getPPtr()->reset(new T(std::forward<Args>(args)...));
     }
 
+    static void factoryObj(T* ptr, bool own = false)
+    {
+        assert(getPPtr()->get() == nullptr && "Have set the Sington instance");
+        getPPtr()->reset(ptr);
+        getPPtr()->get_deleter().m_own = own;
+    }
+
     /**
      * @brief no need to delete for free memory!!
      * this function just for new object
@@ -38,9 +63,9 @@ public:
     }
 
 private:
-    static std::unique_ptr<T>* getPPtr()
+    static std::unique_ptr<T, detail::Delete<T>>* getPPtr()
     {
-        static std::unique_ptr<T> ptr(nullptr);
+        static std::unique_ptr<T, detail::Delete<T>> ptr(nullptr);
         return &ptr;
     }
 };
